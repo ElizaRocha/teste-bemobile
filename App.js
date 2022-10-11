89f304ea8a98eab45573973f73981aa91ce3a94a
Eliza Rocha <iza.neta1234@gmail.com>
2022-10-11 13:42:50 -0300

    taking data of db.json and printing

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { ScrollView, StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default class App extends Component{
+ 
+  constructor (props) {
+    super(props)
+
+  }
+  componentDidMount() {
+    const data = require('./db.json');
+  }
+
+  render() {  
+  const data = require('./db.json');
+  console.log(data.posts)
+  
+
   return (
-    <View style={styles.container}>
-      <Text>Opar</Text>
-      <StatusBar style="auto" />
-    </View>
+    <ScrollView>
+        <View>
+          {data.posts
+            .slice(0, data.posts.id)
+            .map(({ name, cargo, data, telefone, id }, index) => (
+              <View styles={{ flex: 1 }} key={index.toString()}>
+                {console.log(name)}
+                <Text>{id}</Text>
+                 <Text>{name}</Text>
+                 <Text>{telefone}</Text>
+                 <Text>{data}</Text>
+                 <Text>{cargo}</Text>
+              </View>
+            ))}
+        </View>
+      </ScrollView>
   );
+  }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+   
   },
 });
